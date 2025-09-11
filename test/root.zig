dd6845f112d54328525dbe9bf9c4dcf273c8403d
0xpolarzero <0xpolarzero@gmail.com>
2025-09-11 14:07:14 +0200

    test: move to evm/

--- test/root.zig
+++ test/root.zig
@@ -93,7 +93,7 @@ test {
     // _ = @import("evm/snapshot_propagation_tests.zig");
     // C API tests removed - these APIs are not exported from evm module
     // _ = @import("evm/warm_cold_access_tests.zig");
-    _ = @import("selfdestruct_records_test.zig");
+    _ = @import("evm/selfdestruct_records_test.zig");
     
     // Opcode differential tests
     _ = @import("evm/opcodes/all_opcodes.zig");
